40023b248e48605d4a5b31a2cc822c489fd9c41b
tanyapongyulee <pongyulee521@gmail.com>
2013-08-06 08:56:49 -0700

    Update BoardTest.java

--- BoardTest.java
+++ BoardTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 
 public class BoardTest extends TestCase {
 
-  public void testBoard(){
+	public void testBoard(){
 		Board b= new Board("easy.txt");
-		Block[][] board = b.newBoard();	
+		b.buildBoard();	
 		int [] c = b.getSize();
 		assertTrue(4 == c[1]);
 		assertTrue(4==c[0]);
 		
 		Board g = new Board("140x140.txt");
-		Block [][] cord = g.newBoard();
+		g.buildBoard();
 		int [] n = g.getSize();
 		assertTrue(140 == n[1]);
 		assertTrue(140 == n[0]);
@@ -22,15 +22,15 @@ public class BoardTest extends TestCase {
 	}
 	
 	public void testFilledBoard(){
-		Board b=new Board("easy.txt");
-		Block[][] board= b.newBoard();
-		b.placeBlocks();
-		System.out.println(b.getBoard());
+		Board b1=new Board("easy.txt");
+		b1.buildBoard();
+		b1.placeBlocks();
+		System.out.println(b1.printBoard());
 		
-		Board c = new Board("140x140.txt");
-		Block [][] cord = c.newBoard();
-		c.placeBlocks();
-		System.out.println(c.getBoard());
+		Board b2 = new Board("easy2.txt");
+		b2.buildBoard();
+		b2.placeBlocks();
+		System.out.println(b2.printBoard());
 	}
 
 }
