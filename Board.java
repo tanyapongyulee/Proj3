3ee52820d5f5aaf601d6c67c048ac1e8b48d9456
tanyapongyulee <pongyulee521@gmail.com>
2013-08-06 08:57:31 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,35 +1,37 @@
+import java.util.HashSet;
+
 
 public class Board {
-  private InputSource file;
-	private int length;
-	private int width;
+	private InputSource file;
+	private int BoardLength;
+	private int BoardWidth;
 	private Block[][] board;
 	public Board(String fileName){
 		 file=new InputSource(fileName);	//initializes the file name.	
 	}
-	public Block[][] newBoard(){   //returns a 2d array of block objects all initialized to empty. 
+	public void buildBoard(){   //returns a 2d array of block objects all initialized to empty. 
 		String s =file.readLine();
 		String[] v = (String[]) s.split(" ");
-		int length2 = Integer.parseInt(v[0]);
-		int width2 = Integer.parseInt(v[1]);
-		board=new Block[length2][width2];
-		length = length2;
-		width = width2;
-		return board;
+		int  L1= Integer.parseInt(v[0]);
+		int  W1= Integer.parseInt(v[1]);
+		BoardLength=L1;
+		BoardWidth=W1;
+		board=new Block[BoardLength][BoardWidth];
 	}
 	public int[] getSize(){ //returns the parameters of the board, length in index 0, width in index 1.
 		int[] size = new int [2];
-		size[0] = length;
-		size[1] = width;
+		size[0] = BoardLength;
+		size[1] = BoardWidth;
 		return size;
 	}
 	public void placeBlocks(){
 		String input = file.readLine();
 		while(input!=null){
 			String[] v= (String[]) input.split(" ");
+			String name=v[0]+v[1]+v[2]+v[3];
 			int w1 =Integer.parseInt(v[3])- Integer.parseInt(v[1]);
 			int l1 =Integer.parseInt(v[2]) - Integer.parseInt(v[0]);
-			Block b= new Block(w1,l1);
+			Block b= new Block(w1,l1, name);
 			for(int k=Integer.parseInt(v[0]); k<Integer.parseInt(v[2])+1;k++){
 				for(int j=Integer.parseInt(v[1]); j<Integer.parseInt(v[3])+1; j++){
 					board[k][j]=b;
@@ -39,18 +41,32 @@ public class Board {
 			
 		}
 	}
-	public String getBoard(){
+	public String printBoard(){
 		String s="";
-		for (int k = 0; k<length; k++ ){
-			for(int j =0; j<width; j++){
-				s= s+"["+ board[k][j]+","+k+"]"+" ";
+		for (int k = 0; k<BoardLength; k++ ){
+			for(int j =0; j<BoardWidth; j++){
+				s= s+"["+ board[k][j]+"]"+" ";
 			}
 			s=s+"\n";
 		}
 		return s;
 	}
 	
+	public int getEmptyBlocks(){ 
+		//return the null blocks onBoard
+		return 0;
+	}
+	public HashSet moveOk(){
+		// return a set of available blocks to move;
+		return new HashSet();
+	}
+	public void move(Block b){
+		//duh
+	}
+	
+	
 	public boolean possible(Board b2){
+		//using A*algorithm to compute
 		return false;
 	}
 }
