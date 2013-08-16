f46881e278a0f8033a435981d6a7a6494e73f01e
PeterAguiar <paguiar@oberlin.edu>
2013-08-16 00:23:27 -0700

    Update Block.java
    added equals method for match goal.

--- Block.java
+++ Block.java
@@ -5,18 +5,26 @@ public class Block{
 	private int width; //horizontal
 	private int length; //vertical
 	private int size;
-	public Point Top; // x vertical, y horizontal
-	public Point Bottom; // x vertical, y horizontal
-	
-	
-	public Block(int w, int l){
+	protected Point Top; // x vertical, y horizontal
+	protected Point Bottom; // x vertical, y horizontal
+	protected Block(int w, int l){
 		width=w;
 		length=l;
 		size=w*l;
-		
-		
 	}
-	
+	public Block(){
+		width=0;
+		length=0;
+		size=0;
+	}
+	public Block(int w , int l , int x, int y){
+		width =w;
+		length =l;
+		size=w*l;
+		Top= new Point(x,y);
+		Bottom = new Point(x+w,y+l);
+		//System.out.println(Top+" "+Bottom);
+	}
 	public int getWidth(){
 		return width+1;
 	}
@@ -40,9 +48,20 @@ public class Block{
 	public Point getBottom(){
 		return Bottom;
 	}
-	public String toString(){
-		String s="";
-		s=Integer.toString(Top.x)+Integer.toString(Top.y);
-		return s;
+	public boolean equals(Block block2){
+		System.out.println(block2.getTop()+" "+Top);
+		if(Top.x!=block2.getTop().x){
+			return false;
+		}
+		if(Top.y!=block2.getTop().y){
+			return false;
+		}
+		if(getSize()!=block2.getSize()){
+			return false;
+		}
+		return true;
 	}
+	
 }
+
+
