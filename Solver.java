8dea5293ea66e12951108c02afc24010101a051a
tanyapongyulee <pongyulee521@gmail.com>
2013-08-17 10:55:23 -0700

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -10,25 +10,31 @@ public class Solver {
 	    	if (args.length == 3)
 	    		optionsExist = true;
 	    	if (optionsExist) {
+	    		Board init=new Board(args[1]);
+	    		Board goal=new Board(args[2], init.getLength(), init.getWidth());
 	    		if (args[0].substring(0,2).equals("-o")) {
 	    			System.out.println("invalid options parameter");
 	    			System.exit(1);
 	    		}
-	    		switch (args[0]) {
-	    		case "-ooptions":
-	    			System.out.println("WRITE OF OPTIONS STUFF");
-	    			System.out.println("");
-	    			System.exit(0); //don't run any code
-	    		case "-ooption1":
-	    			optionNumber = 1;
-	    		case "-ooption2":
-	    			optionNumber = 2;
-	    		default: 
+	    		if(args[0]=="-ooptions"){
+	    			System.out.println("-oIsOkay: call upon isOkay whenever creating a new board(tray).");
+	   
+	    			System.out.println("-oVisualize: visualize the inital board and final board ");
+	    			System.out.println("-oPrintSteps: visualize the steps taken towards the goal board");
+	    			System.exit(1);
+	    		}
+	    		else if(args[0]=="-oIsOkay"){
+	    		
+	    		}else if(args[0]=="-oVisualize"){
+	    			init.printBoard();
+	    			goal.printBoard();
+	    		}else if(args[0]=="-oPrintSteps");
+	    			
+	    		else{
 	    			System.out.println("invalid option");
 	    			System.exit(1);
 	    		}
-	    		Board init=new Board(args[1]);
-	    		Board goal=new Board(args[2], init.getLength(), init.getWidth());
+	    		
 	    		Greedy solver= new Greedy(init,goal);
 	    		if(solver.possible()){
 	    			solver.printAnswer();
